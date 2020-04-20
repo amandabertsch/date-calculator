5d81dfb357c15242f6d4c25d146a69679413e1ab
amanda <albertsch8@gmail.com>
2020-04-19 20:38:13 -0700

    remove date placeholder

--- src/components/ChangeDate.jsx
+++ src/components/ChangeDate.jsx
@@ -29,15 +29,15 @@ function App(props) {
         <div className="form-group row px-md-0 px-4">
             <div className="col-md-2">
                 <label className="control-label">{props.firstLabel}</label>
-                <input onChange={handleChange} className="form-control" type="date" placeholder="Start Date" name="date"></input>
+                <input onChange={handleChange} className="form-control" type="date" name="date" value={inputs.date}></input>
             </div>
             <div className="col-md-1">
                 <label className="control-label">Week(s)</label>
-                <input onChange={handleChange} className="form-control" type="number" placeholder="0" name="weeks"></input>
+                <input onChange={handleChange} className="form-control" type="number" placeholder="0" name="weeks" value={inputs.weeks}></input>
             </div>
             <div className="col-md-1">
                 <label className="control-label">Day(s)</label>
-                <input onChange={handleChange} className="form-control" type="number" placeholder="0" name="days"></input>
+                <input onChange={handleChange} className="form-control" type="number" placeholder="0" name="days" value={inputs.day}></input>
             </div>
             <div className="col-md-2 mt-auto">
                 <button onClick={submitDate} type="button" className="btn btn-info my-4 my-md-0">Calculate</button>
