d4b801a615b432fea91f27969dd3637b24f41d69
amanda <albertsch8@gmail.com>
2020-04-19 20:30:18 -0700

    make mobile friendly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -75,64 +75,65 @@ function App() {
     }
     
     return(
-    <div className="pl-4">
-      <div className="px-4 mt-2">
-        <h1>Add Time</h1>
-        <ChangeDate 
-          firstLabel="Start Date"
-          lastLabel="End Date"
-          onAdd={addTime}
-          result={endDate}
-        />
-      </div>
-
-      <br></br>
-
-      <div className="px-4 mt-2">
-        <h1>Subtract Time</h1>
-        <ChangeDate 
-          firstLabel="End Date"
-          lastLabel="Start Date"
-          onAdd={subtractTime}
-          result={startDate}
-        />
-      </div>
-
-      <br></br>
-      
-      <div className="px-4 mt-2">
-        <h1>Time Between Dates</h1>
-        <div className="form-group row">
-
-          <div className="col-2">
-              <label className="control-label">Start Date</label>
-              <input onChange={handleChange} className="form-control" type="date" placeholder="Start Date" name="start"></input>
-          </div>
-          <div className="col-2">
-              <label className="control-label">End Date</label>
-              <input onChange={handleChange} className="form-control" type="date" placeholder="Stop Date" name="stop"></input>
-          </div>
-          <div className="col-2 mt-auto">
-                <button onClick={calcDifference} type="button" className="btn btn-info">Calculate</button>
-          </div>
-
-          <div className="col-1 mt-auto">
-              <label className="control-label">Weeks</label>
-              <input value={timeDiff.weeks} className="form-control" readOnly style={{color: "green"}}></input>
-          </div>
-          <div className="col-1 mt-auto">
-              <label className="control-label">Days</label>
-              <input value={timeDiff.days} className="form-control" readOnly style={{color: "green"}}></input>
-          </div>
+    <div id="page-container" className="pl-4 pt-4">
+      <div id="content-wrap">
+        <div className="px-4 mt-2">
+          <h2>Add Time</h2>
+          <ChangeDate 
+            firstLabel="Start Date"
+            lastLabel="End Date"
+            onAdd={addTime}
+            result={endDate}
+          />
         </div>
-          
-      </div>
+
+        <br></br>
+
+        <div className="px-4 mt-2">
+          <h2>Subtract Time</h2>
+          <ChangeDate 
+            firstLabel="End Date"
+            lastLabel="Start Date"
+            onAdd={subtractTime}
+            result={startDate}
+          />
+        </div>
+
+        <br></br>
+        
+        <div className="px-4 mt-2">
+          <h2>Time Between Dates</h2>
+          <div className="form-group row px-md-0 px-4">
+
+            <div className="col-md-2">
+                <label className="control-label">Start Date</label>
+                <input onChange={handleChange} className="form-control" type="date" placeholder="Start Date" name="start"></input>
+            </div>
+            <div className="col-md-2">
+                <label className="control-label">End Date</label>
+                <input onChange={handleChange} className="form-control" type="date" placeholder="Stop Date" name="stop"></input>
+            </div>
+            <div className="col-md-2 mt-auto">
+                  <button onClick={calcDifference} type="button" className="btn btn-info my-4 my-md-0">Calculate</button>
+            </div>
+
+            <div className="col-md-1 mt-auto">
+                <label className="control-label">Weeks</label>
+                <input value={timeDiff.weeks} className="form-control" readOnly style={{color: "green"}}></input>
+            </div>
+            <div className="col-md-1 mt-auto">
+                <label className="control-label">Days</label>
+                <input value={timeDiff.days} className="form-control" readOnly style={{color: "green"}}></input>
+            </div>
+          </div> 
+        </div>
+      </div> 
 
       <footer>
       <p>Made by <a href="https://www.amandabertsch.com"  target="_blank">Amanda Bertsch</a> </p>
       </footer>
-      
 
+      
     </div>
     
 
--- src/components/ChangeDate.jsx
+++ src/components/ChangeDate.jsx
@@ -26,23 +26,23 @@ function App(props) {
     return(
 
     <div>
-        <div className="form-group row">
-            <div className="col-2">
+        <div className="form-group row px-md-0 px-4">
+            <div className="col-md-2">
                 <label className="control-label">{props.firstLabel}</label>
                 <input onChange={handleChange} className="form-control" type="date" placeholder="Start Date" name="date"></input>
             </div>
-            <div className="col-1">
+            <div className="col-md-1">
                 <label className="control-label">Week(s)</label>
                 <input onChange={handleChange} className="form-control" type="number" placeholder="0" name="weeks"></input>
             </div>
-            <div className="col-1">
+            <div className="col-md-1">
                 <label className="control-label">Day(s)</label>
                 <input onChange={handleChange} className="form-control" type="number" placeholder="0" name="days"></input>
             </div>
-            <div className="col-2 mt-auto">
-                <button onClick={submitDate} type="button" className="btn btn-info">Calculate</button>
+            <div className="col-md-2 mt-auto">
+                <button onClick={submitDate} type="button" className="btn btn-info my-4 my-md-0">Calculate</button>
             </div>
-            <div className="col-2 mt-auto">
+            <div className="col-md-2 mt-auto">
                 <label className="control-label">{props.lastLabel}</label>
                 <input value={props.result} className="form-control" readOnly style={{color: "green"}}></input>
             </div>
